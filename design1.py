19847a8b616983687b69b83219b949f236790517
shil09 <2359220297@qq.com>
2019-12-17 11:23:47 +0800

    一些打印星号的例子

---
+++ design1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+n = row = int(input("请输入要打印的行数：")) #行数
+while n >= 0:
+    print("*" * n )
+    n -= 1
---
+++ design2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+n = row = int(input("请输入要打印的行数：")) #行数
+i = 0
+while i <= n:
+    print("*" * i )
+    i = i + 1
