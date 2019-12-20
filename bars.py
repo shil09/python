56c11880861e986034d8bcdb9618ac370e83a6bb
shil09 <2359220297@qq.com>
2019-12-20 09:59:25 +0800

    模块.py

---
+++ bars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+------
+这是一个打印不同分割线你的实力模块
+"""
+def starbar(num):
+    """
+    打印 * 分割线
+    :arg num: 线长
+    """
+    print('*' * num)
+
+def hashbar(num):
+    """
+    打印#分割线
+    :arg num: 线长
+    """
+    print("#" * num)
+
+def simplebar(num):
+    """
+    打印 - 分割线
+    :arg num: 线长
+    """
+    print("-" * num)
